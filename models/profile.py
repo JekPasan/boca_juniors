07e20e6e1c7a755cfdcfaf4af27224b24b461138
unknown <matorurazvan@gmail.com>
2024-02-18 11:59:53 +0200

    added min_length to profile names and passwords

--- models/profile.py
+++ models/profile.py
@@ -3,6 +3,6 @@ from django import forms
 
 class Profile (models.Model):
     email = models.EmailField(max_length=128)
-    username = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=30)
-    password = models.CharField(max_length=50, widget=forms.PasswordInput)
+    username = models.CharField(max_length=30, min_length=3)
+    nickname = models.CharField(max_length=30, min_length=3)
+    password = models.CharField(max_length=50, min_length=6, widget=forms.PasswordInput)
