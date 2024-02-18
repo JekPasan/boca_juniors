eeb9dc2d011a73854035b057251c61fa77c79b78
unknown <matorurazvan@gmail.com>
2024-02-18 14:18:42 +0200

    added custom page for every user

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -6,3 +8,10 @@ def home(request):
 
 def login(request):
     return render(request, "login.html")
+
+@login_required
+def profile(request, username):
+    vars = {
+        "username": username,
+    }
+    return render(request, "profile.html", vars)
